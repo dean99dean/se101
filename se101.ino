bde5073d3ae0ee76fc361da366eec1635bd884e9
dean99dean <dean99dean@gmail.com>
2015-11-04 22:06:03 -0500

    add step counter function

---
+++ se101.ino
@@ -0,0 +1,481 @@
+extern "C" {
+#include <delay.h>
+#include <math.h>
+#include <FillPat.h>
+#include <I2CEEPROM.h>
+#include <LaunchPad.h>
+#include <OrbitBoosterPackDefs.h>
+#include <OrbitOled.h>
+#include <OrbitOledChar.h>
+#include <OrbitOledGrph.h>
+}
+
+#define MODE_0		0
+#define MODE_1		2
+#define MODE_2		1
+#define MODE_3		3
+#define RED_LED   GPIO_PIN_1
+#define BLUE_LED  GPIO_PIN_2
+#define GREEN_LED GPIO_PIN_3
+
+
+extern int xchOledMax; // defined in OrbitOled.c
+extern int ychOledMax; // defined in OrbitOled.c
+
+
+char	chSwtCur;
+char	chSwtPrev;
+bool	fClearOled;
+
+
+
+
+void DeviceInit();
+char CheckSwitches();
+void OrbitSetOled();
+void mode0();
+void mode1();
+void mode2();
+void mode3();
+
+void setup()
+{
+  DeviceInit();
+}
+
+void loop()
+{
+
+  char bDemoState = 0;
+  volatile uint32_t ui32Loop;
+
+  bDemoState = CheckSwitches();
+  for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+  {
+  }
+
+  switch(bDemoState) {
+
+  case MODE_0:
+    mode0();
+    break;
+//  case MODE_1:
+//    mode1();
+//    break;
+//  case MODE_2:
+//    mode2();
+//    break;
+//  case MODE_3:
+//    mode3();
+//    break;
+//  default:
+//    mode0();
+//    break;
+  }
+
+}
+
+
+void DeviceInit()
+{
+  /*
+   * First, Set Up the Clock.
+   * Main OSC		  -> SYSCTL_OSC_MAIN
+   * Runs off 16MHz clock -> SYSCTL_XTAL_16MHZ
+   * Use PLL		  -> SYSCTL_USE_PLL
+   * Divide by 4	  -> SYSCTL_SYSDIV_4
+   */
+  SysCtlClockSet(SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ | SYSCTL_USE_PLL | SYSCTL_SYSDIV_4);
+
+  /*
+   * Enable and Power On All GPIO Ports
+   */
+  //SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOA | SYSCTL_PERIPH_GPIOB | SYSCTL_PERIPH_GPIOC |
+  //						SYSCTL_PERIPH_GPIOD | SYSCTL_PERIPH_GPIOE | SYSCTL_PERIPH_GPIOF);
+
+  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOA );
+  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOB );
+  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOC );
+  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOD );
+  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOE );
+  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOF );
+  /*
+   * Pad Configure.. Setting as per the Button Pullups on
+   * the Launch pad (active low).. changing to pulldowns for Orbit
+   */
+  GPIOPadConfigSet(SWTPort, SWT1 | SWT2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
+
+  GPIOPadConfigSet(BTN1Port, BTN1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
+  GPIOPadConfigSet(BTN2Port, BTN2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
+
+  GPIOPadConfigSet(LED1Port, LED1, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
+  GPIOPadConfigSet(LED2Port, LED2, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
+  GPIOPadConfigSet(LED3Port, LED3, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
+  GPIOPadConfigSet(LED4Port, LED4, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
+
+  /*
+   * Initialize Switches as Input
+   */
+  GPIOPinTypeGPIOInput(SWTPort, SWT1 | SWT2);
+
+  /*
+   * Initialize Buttons as Input
+   */
+  GPIOPinTypeGPIOInput(BTN1Port, BTN1);
+  GPIOPinTypeGPIOInput(BTN2Port, BTN2);
+
+  /*
+   * Initialize LEDs as Output
+   */
+  GPIOPinTypeGPIOOutput(LED1Port, LED1);
+  GPIOPinTypeGPIOOutput(LED2Port, LED2);
+  GPIOPinTypeGPIOOutput(LED3Port, LED3);
+  GPIOPinTypeGPIOOutput(LED4Port, LED4);
+
+  /*
+   * Enable ADC Periph
+   */
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+
+  GPIOPinTypeADC(AINPort, AIN);
+
+  /*
+   * Enable ADC with this Sequence
+   * 1. ADCSequenceConfigure()
+   * 2. ADCSequenceStepConfigure()
+   * 3. ADCSequenceEnable()
+   * 4. ADCProcessorTrigger();
+   * 5. Wait for sample sequence ADCIntStatus();
+   * 6. Read From ADC
+   */
+  ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
+  ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH0);
+  ADCSequenceEnable(ADC0_BASE, 0);
+
+  /*
+   * Initialize the OLED
+   */
+  OrbitOledInit();
+
+  /*
+   * Reset flags
+   */
+  chSwtCur = 0;
+  chSwtPrev = 0;
+  fClearOled = true;
+
+}
+
+/* ------------------------------------------------------------ */
+/***	CheckSwitches()
+ **
+ **	Parameters:
+ **		none
+ **
+ **	Return Value:
+ **		none
+ **
+ **	Errors:
+ **		none
+ **
+ **	Description:
+ **		Return the state of the Switches
+ */
+char CheckSwitches() {
+
+  long 	lSwt1;
+  long 	lSwt2;
+
+  chSwtPrev = chSwtCur;
+
+  lSwt1 = GPIOPinRead(SWT1Port, SWT1);
+  lSwt2 = GPIOPinRead(SWT2Port, SWT2);
+
+  chSwtCur = (lSwt1 | lSwt2) >> 6;
+
+  if(chSwtCur != chSwtPrev) {
+    fClearOled = true;
+  }
+
+  return chSwtCur;
+
+}
+
+//Mode 0
+
+void mode0() {
+  short	dataX;
+  short dataY;
+  short dataZ;
+  char printVal[10];
+
+  char 	chPwrCtlReg = 0x2D;
+  char 	chX0Addr = 0x32;
+   char  chY0Addr = 0x34;
+  char  chZ0Addr = 0x36;
+    char 	rgchReadAccl[] = {
+    0, 0, 0            };
+  char 	rgchWriteAccl[] = {
+    0, 0            };
+     char rgchReadAccl2[] = {
+    0, 0, 0            };
+    
+    char rgchReadAccl3[] = {
+    0, 0, 0            };
+     char szDemo1[]	= {
+    'W','e','l','c','o','m','e' ,' ','2',' ','F','i','t','n','e','s','s'  };
+ /*
+     * Enable I2C Peripheral
+     */
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
+    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C0);
+
+    /*
+     * Set I2C GPIO pins
+     */
+    GPIOPinTypeI2C(I2CSDAPort, I2CSDA_PIN);
+    GPIOPinTypeI2CSCL(I2CSCLPort, I2CSCL_PIN);
+    GPIOPinConfigure(I2CSCL);
+    GPIOPinConfigure(I2CSDA);
+
+    /*
+     * Setup I2C
+     */
+    I2CMasterInitExpClk(I2C0_BASE, SysCtlClockGet(), false);
+
+    /* Initialize the Accelerometer
+     *
+     */
+    GPIOPinTypeGPIOInput(ACCL_INT2Port, ACCL_INT2);
+
+    rgchWriteAccl[0] = chPwrCtlReg;
+    rgchWriteAccl[1] = 1 << 3;		// sets Accl in measurement mode
+    I2CGenTransmit(rgchWriteAccl, 1, WRITE, ACCLADDR);
+
+  
+  /*
+   * If applicable, reset OLED
+   */
+  if(fClearOled == true) {
+    OrbitOledClear();
+    OrbitOledMoveTo(0,0);
+    OrbitOledSetCursor(0,0);
+    fClearOled = false;
+  }
+  
+    /* Initialize the Accelerometer
+     *
+     */
+    GPIOPinTypeGPIOInput(ACCL_INT2Port, ACCL_INT2);
+
+    rgchWriteAccl[0] = chPwrCtlReg;
+    rgchWriteAccl[1] = 1 << 3;		// sets Accl in measurement mode
+    I2CGenTransmit(rgchWriteAccl, 1, WRITE, ACCLADDR);
+   
+   int initial =0;
+   int next =0;
+   int steps = -1;
+     while(CheckSwitches() == MODE_0) {
+
+    /*
+     * Read the X data register
+     */
+    //rgchReadAccl[0] = chX0Addr;
+    //rgchReadAccl2[0] = chY0Addr;
+    rgchReadAccl3[0] = chZ0Addr;
+    
+    //I2CGenTransmit(rgchReadAccl, 2, READ, ACCLADDR);
+   // I2CGenTransmit(rgchReadAccl2, 2, READ, ACCLADDR);
+    I2CGenTransmit(rgchReadAccl3, 2, READ, ACCLADDR);
+    
+   // dataX = (rgchReadAccl[2] << 8) | rgchReadAccl[1];
+   // dataY = (rgchReadAccl2[2] << 8) | rgchReadAccl2[1];
+    dataZ = (rgchReadAccl3[2] << 8) | rgchReadAccl3[1];
+    
+    sprintf(printVal, "Z Accl is : %d  Steps is %d", (int)dataZ,steps);
+    
+    next = dataZ;
+    if(fabs(next - initial) >40) steps++;
+    
+
+     OrbitOledSetCursor(0,0);
+
+     OrbitOledPutString(printVal);
+     
+     OrbitOledUpdate();
+     initial = next;
+     delay(300);
+     printVal[10] = {0};
+     OrbitOledClear();
+    
+  }
+
+}
+
+
+
+
+/* ------------------------------------------------------------ */
+/***	I2CGenTransmit
+ **
+ **	Parameters:
+ **		pbData	-	Pointer to transmit buffer (read or write)
+ **		cSize	-	Number of byte transactions to take place
+ **
+ **	Return Value:
+ **		none
+ **
+ **	Errors:
+ **		none
+ **
+ **	Description:
+ **		Transmits data to a device via the I2C bus. Differs from
+ **		I2C EEPROM Transmit in that the registers in the device it
+ **		is addressing are addressed with a single byte. Lame, but..
+ **		it works.
+ **
+ */
+char I2CGenTransmit(char * pbData, int cSize, bool fRW, char bAddr) {
+
+  int 		i;
+  char * 		pbTemp;
+
+  pbTemp = pbData;
+
+  /*Start*/
+
+  /*Send Address High Byte*/
+  /* Send Write Block Cmd*/
+  I2CMasterSlaveAddrSet(I2C0_BASE, bAddr, WRITE);
+  I2CMasterDataPut(I2C0_BASE, *pbTemp);
+
+  I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+
+  DelayMs(1);
+
+  /* Idle wait*/
+  while(I2CGenIsNotIdle());
+
+  /* Increment data pointer*/
+  pbTemp++;
+
+  /*Execute Read or Write*/
+
+  if(fRW == READ) {
+
+    /* Resend Start condition
+	** Then send new control byte
+	** then begin reading
+	*/
+    I2CMasterSlaveAddrSet(I2C0_BASE, bAddr, READ);
+
+    while(I2CMasterBusy(I2C0_BASE));
+
+    /* Begin Reading*/
+    for(i = 0; i < cSize; i++) {
+
+      if(cSize == i + 1 && cSize == 1) {
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
+
+        DelayMs(1);
+
+        while(I2CMasterBusy(I2C0_BASE));
+      }
+      else if(cSize == i + 1 && cSize > 1) {
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+
+        DelayMs(1);
+
+        while(I2CMasterBusy(I2C0_BASE));
+      }
+      else if(i == 0) {
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
+
+        DelayMs(1);
+
+        while(I2CMasterBusy(I2C0_BASE));
+
+        /* Idle wait*/
+        while(I2CGenIsNotIdle());
+      }
+      else {
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_CONT);
+
+        DelayMs(1);
+
+        while(I2CMasterBusy(I2C0_BASE));
+
+        /* Idle wait */
+        while(I2CGenIsNotIdle());
+      }
+
+      while(I2CMasterBusy(I2C0_BASE));
+
+      /* Read Data */
+      *pbTemp = (char)I2CMasterDataGet(I2C0_BASE);
+
+      pbTemp++;
+
+    }
+
+  }
+  else if(fRW == WRITE) {
+
+    /*Loop data bytes */
+    for(i = 0; i < cSize; i++) {
+      /* Send Data */
+      I2CMasterDataPut(I2C0_BASE, *pbTemp);
+
+      while(I2CMasterBusy(I2C0_BASE));
+
+      if(i == cSize - 1) {
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+
+        DelayMs(1);
+
+        while(I2CMasterBusy(I2C0_BASE));
+      }
+      else {
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_CONT);
+
+        DelayMs(1);
+
+        while(I2CMasterBusy(I2C0_BASE));
+
+        /* Idle wait */
+        while(I2CGenIsNotIdle());
+      }
+
+      pbTemp++;
+    }
+
+  }
+
+  /*Stop*/
+
+  return 0x00;
+
+}
+
+/* ------------------------------------------------------------ */
+/***	I2CGenIsNotIdle()
+ **
+ **	Parameters:
+ **		pbData	-	Pointer to transmit buffer (read or write)
+ **		cSize	-	Number of byte transactions to take place
+ **
+ **	Return Value:
+ **		TRUE is bus is not idle, FALSE if bus is idle
+ **
+ **	Errors:
+ **		none
+ **
+ **	Description:
+ **		Returns TRUE if the bus is not idle
+ **
+ */
+bool I2CGenIsNotIdle() {
+
+  return !I2CMasterBusBusy(I2C0_BASE);
+
+}
